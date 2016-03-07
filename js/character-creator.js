a4facf8cc5ed5457561c3309f9f380a3a9e24a2f
dillon_diep <dd13282@my.bristol.ac.uk>
2016-03-07 03:36:03 +0000

    add button base script

--- js/character-creator.js
+++ js/character-creator.js
@@ -6,53 +6,44 @@ $(document).ready(function() {
 init = function() {
 	var characterCreator = document.getElementById("character-creator");
 	var characterCanvas = document.getElementById("character");
-	var ui = document.getElementById("ui");
 
 	// resizing listeners for responsive canvas
 	resizeCanvas(characterCreator);
 	resizeCanvas(characterCanvas);
-	resizeCanvas(ui);
 	window.addEventListener('resize', function() {
     	resizeCanvas(characterCreator);
     	resizeCanvas(characterCanvas);
-    	resizeCanvas(ui);
 	}, false);
-
-	var uiContext = ui.getContext("2d");
-	uiContext.mouse = {
-        x: 0,
-        y: 0,
-        clicked: false,
-        down: false
-    };
-	ui.addEventListener("mousemove", function(e) {
-		uiContext.mouse.x = e.offsetX;
-		uiContext.mouse.y = e.offsetY;
-		uiContext.mouse.clicked = (e.which == 1 && !uiContext.mouse.down);
-        uiContext.mouse.down = (e.which == 1);
-        console.log(uiContext.mouse);
-	});
-
-	ui.addEventListener("mousedown", function(e) {
-        uiContext.mouse.clicked = !uiContext.mouse.down;
-        uiContext.mouse.down = true;
-        console.log("down");
-    });
-
-    ui.addEventListener("mouseup", function(e) {
-        uiContext.mouse.down = false;
-        uiContext.mouse.clicked = false;
-    });
 
 	loadImages(backgroundImages, function() {
 		for (var i = 0; i < backgroundImages.length; i++) 
 			draw(characterCreator, backgroundImages[i], characterCreator.width/2, characterCreator.height/2, characterCreator.width, characterCreator.height);
 	});
 	drawCharacter(characterCanvas, myCharacter, characterCanvas.width/1.3, characterCanvas.height/2., characterAspectRatio*(characterCanvas.height/1.4) , (characterCanvas.height/1.4));
-
-	// draw UI
-
+	
+	setupUI();
+	placeUI(characterCreator);
 };
+
+setupUI = function() {
+	// UI
+	document.querySelectorAll('.ui-button')[0].addEventListener('click', function(event) {
+	   console.log('Button clicked'); 
+	});
+}
+
+placeUI = function(canvas) {
+	var rect = canvas.getBoundingClientRect();
+	console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+	var uibutton = document.querySelectorAll('.ui-button')[0];
+	uibutton.style.width = canvas.width/10+'px';
+	uibutton.style.height = canvas.height/10+'px';
+
+	uibutton.style.left = x+'px';
+	uibutton.style.top = y+'px';
+
+}
 
 // variables
 var characterAspectRatio = 191.2/236;
@@ -64,6 +55,9 @@ var backgroundImages = [
 var cachedImages = {};
 // character class
 character = function() {
+	this.characterName = "";
+	this.ownerName = "";
+
 	this.hair = {type: "hair_medium", hueShift: 0, 
 				xOffset: 0, yOffset: 0, xScale: 1, yScale: 1};
 	this.nose = {type: "nose_default", hueShift: 0, 
@@ -161,11 +155,6 @@ function onLoadedCallback(src, batch)
 	}
 }
 
-getAspectRatio = function(width, height)
-{
-	return width/height;
-}
-
 draw = function(canvas, src, x, y, width, height) {
 	var context = canvas.getContext("2d");
 	context.drawImage(cachedImages[src], x-width/2, y-height/2, width, height);
@@ -173,17 +162,18 @@ draw = function(canvas, src, x, y, width, height) {
 
 // event function that makes canvas responsive
 resizeCanvas = function(canvas) {
+		canvas.style.marginLeft = "0px";
+		canvas.style.marginTop = "0px";
 		var gameContainer = document.getElementById("main");
 		var context = canvas.getContext("2d");
-
 		var dataURL = canvas.toDataURL("image/png", 1.0);
 
-		var flexVal = 14.0;
-		var aspectRatio = 16.0/10.0;
-		//var resizeWidth = gameContainer.clientWidth;
+		var flexVal = 12.0;
+		var aspectRatio = 16.0/12.0;
 		var resizeWidth = window.innerWidth*(10.0/flexVal);
 		var resizeHeight = window.innerHeight*(10.0/flexVal);
 		var newAspectRatio = resizeWidth/resizeHeight;
+
 		// resize, retaining aspect ratio
 		if (newAspectRatio > aspectRatio) {
 			// shrink by width
@@ -192,66 +182,17 @@ resizeCanvas = function(canvas) {
 			// shrink by height
 			resizeHeight = resizeWidth / aspectRatio;
 		}
-
+	
 		canvas.width = resizeWidth;
 		canvas.height = resizeHeight;
-		canvas.style.paddingLeft = (gameContainer.clientWidth-resizeWidth)/2 + 'px';
-		canvas.style.paddingTop = '0.5em';
-		//gameContainer.style.fontSize = (resizeWidth / 400) + 'em';
-		//draw(canvas, [dataURL], 0, 0, canvas.width, canvas.height);
+
+		canvas.style.marginLeft = (gameContainer.clientWidth-resizeWidth)/2 + 'px';
+		canvas.style.marginTop = (gameContainer.clientHeight-resizeHeight)/2 + 'px';
+
+
 		var image = new Image();
 		image.src = dataURL;
 		image.onload = function() {
 			context.drawImage(image, 0, 0, canvas.width, canvas.height);
 		};
 };
-
-//********** this.parent() for inheritance
-
-drawSprite = function(spriteName, x, y) {
-
-}
-
-// declare an base entity class for game objects
-Entity = function() {
-	var position = {x: 0, y: 0};
-	var size = {x: 0, y: 0};
-	var isAlive = true;
-	var spriteName = null;
-	var zIndex = 0;
-
-	update = function(){};
-	draw = function(){
-		// subtract pivot to centre around given position
-		drawSprite(this.spriteName, this.position.x.round() - this.hsize.x, this.position.y.round() - this.hsize.y);
-	};
-};
-
-// declare the game engine class that handles main game logic and loop
-GameEngine = function() {
-	entities = new function(){};
-	factory = new function(){};
-
-	spawnEntity = function(typename) {
-		return new (factory[typename])();
-	}
-
-	// the update loop goes through all entities and calls update
-	update = function() {
-		for (var i = 0; i < this.entities.length; i++)
-		{
-			var entity = this.entities[i];
-			entity.update();
-		}
-	}
-	// engine draw function
-	draw = function() {
-		// draw map
-
-		// draw entities
-
-
-	}
-}
-
-//gameEngine = new GameEngine();
