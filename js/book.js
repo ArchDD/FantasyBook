97299daa3d0b2ab8018a97ed1318064425516a87
Jason Haciepiri <jas_234@hotmail.co.uk>
2016-03-07 01:57:02 +0000

    Flipbook methods, including empty server handlers, and page turning.

--- js/book.js
+++ js/book.js
@@ -13,8 +13,8 @@ $(window).on("load", function() {
         var mainHeight = $("#main").height();
         var $flipbook = $("#flipbook");
 
-        $flipbook.css({ height:8 * mainHeight/10,
-                        top:mainHeight/10
+        $flipbook.css({ height:7 * mainHeight/10,
+                        top:1.5*mainHeight/10
                      });
 
         for (var key in books) {
@@ -59,17 +59,17 @@ $(window).on("load", function() {
     }
 
     var books = {
-        "Lord-of-the-Rings" : {
-            height: 400, width: 96, colour: "#2a6f41", texture: "leather"
+        "book1" : {
+            bookTitle: "Lord of the Rings", desc: "[Theme description]", pages: 5, height: 400, width: 96, colour: "#2a6f41", texture: "leather"
         },
-        "Alice-in-Wonderland" : {
-            height: 400, width: 48, colour: '#42548a', texture: "leather"
+        "book2" : {
+            bookTitle: "Alice in Wonderland", desc: "[Theme description]", pages: 5, height: 400, width: 48, colour: '#42548a', texture: "leather"
         },
-        "A-Hitchhikers-Guide-to-the-Galaxy" : {
-            height: 400, width: 36, colour: '#661919', texture: "leather"
+        "book3" : {
+            bookTitle: "The Colour of Magic", desc: "[Theme description]", pages: 5, height: 400, width: 36, colour: '#661919', texture: "leather"
         },
-        "Coraline" : {
-            height: 400, width: 50, colour: '#4d3960', texture: "leather"
+        "book4" : {
+            bookTitle: "Coraline", desc: "[Theme description]", pages: 5, height: 400, width: 50, colour: '#4d3960', texture: "leather"
         }
     }
 
@@ -82,41 +82,140 @@ $(window).on("load", function() {
         }
     }
 
-    // Attach mouse over event for books created at runtime
-    var intervalId;
-    $('body').on('mouseover', '.book', function() {
-        intervalId = setInterval(function() {
-            console.log("HEEYy");
-        }, 1000);
-    });
-
-    $('body').on('mouseout', '.book', function() {
-        clearInterval(intervalId);
-    });
-
     var bookIsOpen = false;
+    var pageNumber = 0;
+    var currentBook = null;
 
     // Open flipbook and exit book shelf
     $("body").on("click", ".book", function() {
         if(!bookIsOpen) {
+
             bookIsOpen = true;
+            currentBook = books[this.id];
+
+            // First page setup
+            pageNumber = 0;
+
+            $("#right_page .page-title").html(currentBook.bookTitle);
+            $("#right_page .page-title").show();
+            $("#left_page .page-title").hide();
+            $("#left_page .page-image").hide();
+
+            $("#left_page .page-content").html("");
+            $("#right_page .page-content").html(currentBook.desc);
+
+            $("#left_page .page-number").html("");
+            $("#right_page .page-number").html(pageNumber+1);
+
             // Make flipbook visible
             $("#flipbook-container").css("display","flex");
             $("#flipbook-container").css("visibility","visible");
             $("#bookshelf-container").css("display","none");
             $("#bookshelf-container").css("visibility","hidden");
-            
-            // Set title to book title
-            //var front_cover = document.getElementById("front_cover");
-            //front_cover.innerHTML = this.id;
-            // Set colour to book colour
-            //$('.hard').css("background",books[this.id].colour);
-            //$('.hard').css("background","rgba(0,0,0,1)");
-
-            //$('.hard').append(createBookCanvas(settings));
-            // Load content
         }
     });
+
+    $("body").on("click", "#right_page", function(e) {
+        nextPage();
+    });
+
+    $("body").on("click", "#left_page", function(e) {
+        previousPage();
+    });
+
+    // contacts server to get the event name for the page
+    function getEventTitle(book, pageNumber) {
+        return "Event";
+    }
+
+    // contacts server to get either an event description or past event result
+    function getEventContent(book, pageNumber) {
+        return "Event description";
+    }
+
+    // contacts server to get active status of the event 
+    function isEventComplete(book, pageNumber) {
+        return "False";
+    }
+
+    // contacts server to get active status of the event 
+    function getEventChoices(book, pageNumber) {
+
+        var choices = {
+            choice1 : "Go",
+            choice2 : "Leave"
+        }
+
+        return choices;
+    }
+
+    function updatePage(pageId, book, pageNumber) {
+        $(pageId+" .page-title").html(getEventTitle(currentBook, pageNumber));
+        $(pageId+" .page-content").html(getEventContent(currentBook, pageNumber));
+        $(pageId+" .page-number").html(pageNumber);
+    }
+
+    // Change page content to next page
+    function nextPage() {
+        // If there are more pages left
+        if(pageNumber + 2 < currentBook.pages) {
+            pageNumber += 2;
+            // show left title and image
+            $("#left_page .page-title").show();
+            $("#left_page .page-image").show();
+
+            // Update content of the left page
+            // Contact server to get either new event, saved result, or empty string
+            updatePage("#left_page",currentBook,pageNumber);
+
+            // Set right hand page to server content if it is not the end of the book
+            if(pageNumber + 1 < currentBook.pages) {
+                updatePage("#right_page",currentBook,pageNumber+1);
+            }
+            // otherwise empty string
+            else  {
+                $("#right_page .page-title").html("");
+                $("#right_page .page-content").html("");
+                $("#right_page .page-image").hide();
+                $("#right_page .page-number").html("");
+            }           
+            
+        }
+    }
+
+    // Change page content to previous page
+    function previousPage() {
+        // If there are more pages left
+        if (pageNumber -2 >= 0) {
+            pageNumber -= 2;
+            
+            $("#right_page .page-image").show();
+            // If it is the first page, the left is blank
+            // and the right page has the title and setting description
+            if(pageNumber == 0) {
+                // blank left page
+                $("#left_page .page-title").hide();
+                $("#left_page .page-image").hide();
+
+                $("#left_page .page-content").html("");
+                $("#left_page .page-number").html("");
+
+                // Write title
+                $("#right_page .page-title").html(currentBook.bookTitle);
+                // write book description
+                $("#right_page .page-content").html(currentBook.desc);
+                $("#right_page .page-number").html(pageNumber+1);
+            }
+            // otherwise
+            else {
+                // update left page
+                updatePage("#left_page",currentBook,pageNumber);
+                updatePage("#right_page",currentBook,pageNumber+1);
+            }
+        }
+    }
+
+
     // Do not propagate click to the flipbook container beneath
     $("body").on("click", "#flipbook", function(e) {
         e.stopPropagation();
@@ -165,15 +264,15 @@ $(window).on("load", function() {
         settings = $.extend(true, {
             height: 300,
             width: 48,
-            spineText: key,
+            bookTitle: key,
             colour: "#02B8Be"
         }, settings);
 
         //get height of the shelf
         var shelfHeight = $('#bookshelf').height();
         //create text and book elements
-        var $text = $('<span class="spineText unselectable">'+settings.spineText+'</span>');
-        var $book = $('<div class="book" id="'+settings.spineText+'"/>');
+        var $text = $('<span class="spineText unselectable">'+settings.bookTitle+'</span>');
+        var $book = $('<div class="book" id="'+key+'"/>');
         //apply book settings
         $book.height(settings.height);
         $book.width(settings.width);
